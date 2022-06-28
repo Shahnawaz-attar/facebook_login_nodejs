917ea41e0b49fbd86a8527cffea71aa8d7693840
Shahnawazatr <Shahnawaz@skills-agency.com>
2022-06-28 11:27:43 +0530

    facebook login

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+	'facebookAuth': {
+		'clientID':  '1075024926333878', // your App ID
+		'clientSecret':  '08ef6d724cd6f2d956c86e75928fa15a', // your App Secret
+		'callbackURL':  'http://localhost:3000/auth/facebook/callback'
+	}
+}
